8758be5ff56f2c470e7d85e852e7436275079820
SubhdeepChakraborty <chakrabortysubhdeep@icloud.com>
2025-06-22 22:46:50 +0530

    added new comp and layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import { Portfolio, Hero, Contact, Services } from "./components"
 
 const App = () => {
   return (
-    <div>
+    <div className="container">
+      <section id="home">
       <Hero />
+      </section>
+      <section id="portfolio">
       <Portfolio />
+      </section>
+      <section id="services">
       <Services />
+      </section>
+      <section id="contact">
       <Contact />
+      </section>
     </div>
-  )
+  );
 }
 
 export default App
